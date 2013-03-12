67da949268359ba82fd7b20752a64a919c1cd597
Chethan Bandi <chethanbandi@gmail.com>
2013-03-12 14:05:29 +0530

    initial commit - first version of PNR query RESTful service using flask

---
+++ pnr.py
@@ -0,0 +1,131 @@
+# TODO
+# handle no internet connection exception
+# handle internet timeout exception
+# handle html parse exception
+# D0 PNR validations number, length etc
+
+from lxml import html
+import httplib, urllib
+
+from constants import *
+
+class PNRClass:
+   pnr = {}
+
+   def __init__(self):
+      self.pnr["Status"] = {"code": 0, "message": "None"}
+
+   def getJourneyDetails(self, data):
+      trElements = data.getchildren()
+       
+      journeyDetails = trElements[2].getchildren()
+      self.pnr["TrainNumber"] = journeyDetails[0].text
+      self.pnr["TrainName"]   = (journeyDetails[1].text).lstrip().rstrip()
+      self.pnr["BoardingDate"] = journeyDetails[2].text
+      self.pnr["From"] = journeyDetails[3].text
+      self.pnr["To"] = journeyDetails[4].text
+      self.pnr["ReservedUpto"] = journeyDetails[5].text
+      self.pnr["BoardingPoint"] = journeyDetails[6].text
+      self.pnr["class"] = journeyDetails[7].text
+   
+   def getPassengerDetails(self, data):
+      # this is actual passenger information
+      trElements = data.getchildren()
+      self.pnr["ChartingStatus"] = (trElements[-2].getchildren()[1].text).lstrip().rstrip()
+       
+      passenger = []
+      for tr in trElements[1:-2]:
+        passDict = {}
+        td = tr.getchildren()
+        passDict["Name"] = td[0].getchildren()[0].text
+        passDict["BookingStatus"] = (td[1].getchildren()[0].text).lstrip().rstrip()
+        passDict["CurrentStatus"] = td[2].getchildren()[0].text
+        passenger.append(passDict)
+      self.pnr["passenger"] = passenger
+        
+   def parseSuccess(self, root):
+     successDataXpath = "//table[@width='100%' and @border='0' and @cellpadding='0' and @cellspacing='1' and @class='table_border']"
+     dataElements = root.xpath(successDataXpath)
+   
+     for data in dataElements:
+       if data.get("id") is None:
+         # this is train information
+         self.getJourneyDetails(data)
+       else:
+         self.getPassengerDetails(data)
+         
+   def parseFailure(self, root):
+     failureXpath = "//td[@class='inside_heading_text' and @colspan='4' and @align='center' and @valign='top']"
+     serverErrorXpath = "//h1"
+     serverErrorString = "Sorry, This particular service is unavailable at this time!!!"
+     
+     e = root.xpath(serverErrorXpath)
+     if len(e) == 1:
+       if e[0].text == serverErrorString:
+         self.pnr["Status"]["message"] = serverErrorString
+         self.pnr["Status"]["code"] = STATUS_FAIL
+         return
+     
+     e = root.xpath(failureXpath)
+     p = e[0].getparent().getparent().getparent()
+     c = p.getchildren()
+     
+     self.pnr["Status"]["code"] = STATUS_FAIL
+     self.pnr["Status"]["message"] = c[2].text
+   
+   
+   def parseHtml(self, result):
+      root = html.fromstring(result)
+      #root = html.parse("pnr_result.html")
+      #root = html.parse("pnr_fail.html")
+      successXpath = "//td[@class='Enq_heading' and @colspan='4' and @align='center' and @valign='top']"
+      
+      
+      enqHeading = root.xpath(successXpath)
+      if len(enqHeading) == 1:
+        self.parseSuccess(root)
+      else:
+        self.parseFailure(root)
+   
+   def queryWeb(self, pnrNumber):
+     return_object = {}
+     lccp_pnrno1 = pnrNumber
+     params = urllib.urlencode({'lccp_pnrno1': lccp_pnrno1, 'submitpnr': 'Get Status'})
+     headers = {"Content-type": "application/x-www-form-urlencoded",
+                "Host": "www.indianrail.gov.in",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "en-us,en;q=0.5",
+                "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
+                "Keep-Alive": "115",
+                "Connection": "keep-alive",
+                "Referer": "http://www.indianrail.gov.in/pnr_stat.html",
+                "Accept": "text/plain"}
+     host = 'www.indianrail.gov.in'
+     path = '/cgi_bin/inet_pnrstat_cgi.cgi'
+     conn = httplib.HTTPConnection(host,80,timeout=500)
+     return_object = {}
+     return_object['status'] = 'OK'
+     return_object['data'] = {}
+     try :
+       conn.request("POST", path, params, headers)
+       response = conn.getresponse()
+       data = response.read()
+       conn.close()
+       return data
+     except:
+       pass
+   
+   def queryPnr(self, pnrno):
+   
+     self.pnr["PNR"] = pnrno
+     result = self.queryWeb(pnrno)
+     self.parseHtml(result)
+     return self.pnr
+   
+      
+if __name__ == '__main__':
+  for pnrno in [4249727205, 4246884564, 4560301997, 4802979502, 4703019444, 4802979502, 4344195697, 4444195935]:
+      pnrobject = PNRClass()
+      print pnrobject.queryPnr(pnrno)
+   
---
+++ views.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from pnr import PNRClass
+from constants import *
+
+app = Flask(__name__)
+
+@app.route('/v1/pnr/<int:pnrno>', methods=['GET'])
+def queryPnr(pnrno):
+	pnrobject = PNRClass()
+	result = pnrobject.queryPnr(pnrno)
+	return jsonify(result)
+
+@app.errorhandler(404)
+def pageNotFound(error):
+	return jsonify({"Status": {"code" : STATUS_INVALID_RESOURCE, "message" : "Invalid resource uri"}})
+
+if __name__ == '__main__':
+	app.debug = False
+	app.run()
+
